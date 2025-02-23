5a3f88137363e17242b5ad28ba102f8d3837e9c5
Alissa <a.delattre@student.isartdigital.com>
2025-02-23 18:35:48 +0100

    Juicy title animation
    Added an animation player to the title to have a juicy animation when the game starts

---
+++ PROJECT/Scripts/AuthentificationJuiciness.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+// Author : Alissa Delattre
+
+namespace Com.IsartDigital.ProjectName {
+	
+	public partial class AuthentificationJuiciness : Node
+	{
+		[Export] private AnimationPlayer wooly;
+		[Export] private AnimationPlayer way;
+		public override void _Ready()
+		{
+			startAnim();
+		}
+
+		public override void _Process(double pDelta)
+		{
+			float lDelta = (float)pDelta;
+
+		}
+
+		private void startAnim()
+		{
+			wooly.Play("Title");
+			wooly.AnimationFinished += EndAnim;
+		}
+
+        private void EndAnim(StringName animName)
+		{
+			way.Play("Way");
+		}
+		protected override void Dispose(bool pDisposing)
+		{
+
+		}
+	}
+}
