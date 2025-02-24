aa6409c69ed53dde6ccaeeb70b9ac6e3ba20d671
Clément DUCROQUET <clement.ducro08@gmail.com>
2025-02-24 19:45:41 +0100

    [WIP] UserManager methods commented.
    The methods already included into UserManager.cs are commented by now!

--- PROJECT/Scripts/Managers/UserManager.cs
+++ PROJECT/Scripts/Managers/UserManager.cs
@@ -45,8 +45,8 @@ namespace Com.IsartDigital.WoolyWay.Managers
 
 			//userDataPath = userDataFile.ResourcePath; // convert into string -> remove root (as string) -> ../{path}
 
-			/*GD.Print("CreateUser");
-			CreateUser(new Dictionary<string, List<string>>() { { "usernameblabla", new List<string>() { "thispassword", "thissalt" } } });*/
+			GD.Print("CreateUser");
+			CreateUser(new Dictionary<string, List<string>>() { { "usernameblabla", new List<string>() { "thispassword", "thissalt" } } });
 		}
 
 		public override void _Process(double pDelta)
@@ -54,13 +54,20 @@ namespace Com.IsartDigital.WoolyWay.Managers
 			float lDelta = (float)pDelta;
 		}
 
+		/// <summary>
+		/// Stores log in information of a user into a json file.
+		/// </summary>
+		/// <param name="pUserData">A dictionary of which the key matches the username, and the value is a List containing both hached password and password salt.</param>
 		public void CreateUser(Dictionary<string, List<string>> pUserData)
 		{
-			JsonSerializerOptions lJsonOptions;
-			string lUserJsonData; string lJsonData;
+            #region DECLARATIONS
+            JsonSerializerOptions lJsonOptions;
+			string lUserJsonData;
+			string lGlobalJsonData;
 			List<UserData> lAllUsersData;
 			
 			UserData lNewUser = new UserData();
+            #endregion
 
             foreach (KeyValuePair<string, List<string>> lData in pUserData)
 			{
@@ -70,19 +77,24 @@ namespace Com.IsartDigital.WoolyWay.Managers
             }
 
             lJsonOptions = new JsonSerializerOptions();
-            lJsonOptions.IncludeFields = true;
-			lJsonOptions.WriteIndented = true;
+            lJsonOptions.IncludeFields = true; //Default is false. If false, UserData fields are not included into the json serialization and cause the class instance to be empty.
+            lJsonOptions.WriteIndented = true;
 
-			lUserJsonData = JsonSerializer.Serialize(lNewUser, lJsonOptions);
+			lUserJsonData = JsonSerializer.Serialize(lNewUser, lJsonOptions); //Makes the class instance a json object.
 
-			lAllUsersData = JsonSerializer.Deserialize<List<UserData>>(GetAllUsersData(userDataPath), lJsonOptions);
+			lAllUsersData = JsonSerializer.Deserialize<List<UserData>>(GetAllUsersData(userDataPath), lJsonOptions); //Makes the json object(s) a UserData class instance.
 			lAllUsersData.Add(lNewUser);
 
-			lJsonData = JsonSerializer.Serialize(lAllUsersData, lJsonOptions);
+			lGlobalJsonData = JsonSerializer.Serialize(lAllUsersData, lJsonOptions);
 
-            OverwriteJsonFile(userDataPath, lJsonData);
+            OverwriteJsonFile(userDataPath, lGlobalJsonData);
         }
 
+		/// <summary>
+		/// Returns data from a file according to its path (pPathOfFileToRead) as a json serialized string value.
+		/// </summary>
+		/// <param name="pPathOfFileToRead">Path of the file to be open and read. Returned value will be equal to the content of this file.</param>
+		/// <returns></returns>
 		private string GetAllUsersData(string pPathOfFileToRead)
 		{
 			string lData;
@@ -91,7 +103,12 @@ namespace Com.IsartDigital.WoolyWay.Managers
 			return lData;
 		}
 
-		private void OverwriteJsonFile(string pPathOfFileToOverwrite, string pTextToWrite)
+        /// <summary>
+        /// Overwrites a file according to its path (pPathOfFileToOverwrite) with pTextToWrite data.
+        /// </summary>
+        /// <param name="pPathOfFileToOverwrite">Path of the file to be open and overwritten.</param>
+        /// <param name="pTextToWrite">Text to write into the file. Former data will be automatically deleted if not included into this param.</param>
+        private void OverwriteJsonFile(string pPathOfFileToOverwrite, string pTextToWrite)
 		{
 			File.WriteAllText(pPathOfFileToOverwrite, pTextToWrite);
         }
