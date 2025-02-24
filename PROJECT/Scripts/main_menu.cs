717c8a6345c6a212510019f5f38be2f7d7c247c4
LIM Julien <j.lim@student.isartdigital.com>
2025-02-24 22:54:31 +0100

    MainMenu "done", placeholder PauseMenu, Started Settings interface

--- PROJECT/Scripts/main_menu.cs
+++ PROJECT/Scripts/main_menu.cs
@@ -3,10 +3,44 @@ using System;
 
 // Author : Julien Lim
 
-namespace Com.IsartDigital.ProjectName {
-	
-	public partial class main_menu : Control
-	{
-		
-	}
+namespace Com.IsartDigital.ProjectName
+{
+
+    public partial class main_menu : Control
+    {
+        //private CanvasLayer canvasLayer;
+
+
+        public override void _Ready()
+        {
+            //canvasLayer = GetNode<CanvasLayer>("CanvasLayer");
+            GetNode<Button>("VBoxContainer/StartButton").Pressed += OnStartButtonPressed;
+            GetNode<Button>("VBoxContainer/SettingsButton").Pressed += OnSettingsButtonPressed;
+            GetNode<Button>("VBoxContainer/CreditsButton").Pressed += OnCreditsButtonPressed;
+            GetNode<Button>("VBoxContainer/QuitButton").Pressed += OnQuitButtonPressed;
+        }
+
+        private void OnStartButtonPressed()
+        {
+            GD.Print("Start button pressed");
+        }
+
+        private void OnSettingsButtonPressed()
+        {
+            GD.Print("Settings button pressed");
+        }
+
+        private void OnCreditsButtonPressed()
+        {
+            GD.Print("Credits button pressed");
+        }
+
+        private void OnQuitButtonPressed()
+        {
+            GD.Print("Quit button pressed");
+        }
+    }
+
 }
+
+
