637dbdf85c6e484ef2a87e540329e32a3b708f7c
Alissa <a.delattre@student.isartdigital.com>
2025-03-03 10:04:42 +0100

    Merge debug
    added missing godot icon after the merge

---
+++ PROJECT/Scripts/PathFindingCell.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+// Author : Alissa Delattre
+
+namespace Com.IsartDigital.ProjectName {
+	
+	public partial class PathFindingCell : Node
+	{
+		int posX;
+		int posY;
+		int cost;
+		int distance;
+		int costDistance;
+		PathFindingCell lastCell;
+		public override void _Ready()
+		{
+
+		}
+
+		public override void _Process(double pDelta)
+		{
+			float lDelta = (float)pDelta;
+
+		}
+
+		private void SetDistance(int pTargetX, int pTargetY)
+		{
+			distance = Mathf.Abs(pTargetX - posX) + Mathf.Abs(pTargetY - posY);
+		}
+
+		protected override void Dispose(bool pDisposing)
+		{
+
+		}
+	}
+}
--- PROJECT/Scripts/Pathfinding.cs
+++ PROJECT/Scripts/Pathfinding.cs
@@ -5,37 +5,63 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public partial class Pathfinding : Node2D
+
+namespace Com.IsartDigital.ProjectName
 {
-	public override void _Ready()
-	{
-	}
+    public partial class Pathfinding : Node2D
+    {
+        public override void _Ready()
+        {
+        }
 
-	private float DistanceTo(Tile pTile, Tile pArrivalTile)
-	{
-		float lDistance = MathF.Abs((GridManager.TileDict[pTile].X - GridManager.TileDict[pArrivalTile].X)) + MathF.Abs((GridManager.TileDict[pTile].Y - GridManager.TileDict[pArrivalTile].Y));
-		return lDistance;
-	}
+        //private float DistanceTo(Tile pTile, Tile pArrivalTile)
+        //{
+        //	float lDistance = MathF.Abs((GridManager.TileDict[pTile].X - GridManager.TileDict[pArrivalTile].X)) + MathF.Abs((GridManager.TileDict[pTile].Y - GridManager.TileDict[pArrivalTile].Y));
+        //	return lDistance;
+        //}
 
-	private void Path(Tile pStartPos, Tile pEndPos)
-	{
-		List<Tile> lToSearch = new List<Tile>() {pStartPos};
-		List<Tile> lProcessed = new List<Tile>();
+        //private void Path(Tile pStartPos, Tile pEndPos)
+        //{
+        //	List<Tile> lToSearch = new List<Tile>() {pStartPos};
+        //	List<Tile> lProcessed = new List<Tile>();
 
-		while(lToSearch.Any())
-		{
-			Tile lCurrent = lToSearch[0];
-			foreach(Tile lTile in lToSearch)
-			{
-				float lDistance = DistanceTo(lTile, pEndPos);
-				float lCurentDistance = DistanceTo(lCurrent, pStartPos);
-				if(lDistance < lCurentDistance || lDistance == lCurentDistance && )
-			}
+        //	while(lToSearch.Any())
+        //	{
+        //		Tile lCurrent = lToSearch[0];
+        //		foreach(Tile lTile in lToSearch)
+        //		{
+        //			float lDistance = DistanceTo(lTile, pEndPos);
+        //			float lCurentDistance = DistanceTo(lCurrent, pStartPos);
+        //			if(lDistance < lCurentDistance || lDistance == lCurentDistance && )
+        //		}
 
-		}
-	}
+        //	}
+        //}
 
-	public override void _Process(double delta)
-	{
-	}
+        //private List<Vector2> GetPath(List<List<Content>> pGrid, int pStartPosX, int pStartPosY, int pTargetPosX, int pTargetPosY)
+        //{
+
+        //}
+
+        //private List<PathFindingCell> GetWalkableCell(List<List<Content>> pGrid, PathFindingCell pCurrentCell, PathFindingCell pTargetCell)
+        //{
+
+        //}
+
+        //private bool TestVisitedCell(PathFindingCell pWalkableCell, List<PathFindingCell> pVisitedCell)
+        //{
+
+        //}
+
+        //private PathFindingCell IsActiveCellWalkable(PathFindingCell pWalkableCell, List<PathFindingCell> pActiveCells)
+        //{
+
+        //}
+        //public override void _Process(double delta)
+        //{
+
+
+
+        //}
+    }
 }
