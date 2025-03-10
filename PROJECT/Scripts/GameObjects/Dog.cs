1abad51ad6a483931415e140bb713ae311a71c2a
Bagnara Tom <t.bagnara@student.isartdigital.com>
2025-03-10 14:28:09 +0100

    Merge Movement on Dev Crash Fix Movement and Dog fully functional. Place holder modulate to differentiate different Sheep and Tiles

--- PROJECT/Scripts/GameObjects/Dog.cs
+++ PROJECT/Scripts/GameObjects/Dog.cs
@@ -1,26 +1,31 @@
-﻿using Godot;
-using System;
+using Godot;
 
-namespace Com.IsartDigital.WoolyWay.GameObjects;
 
-public partial class Dog : Obstacle
+// Author : Tom Bagnara
+
+namespace Com.IsartDigital.WoolyWay.GameObjects
 {
-    public Vector2I Direction { get; private set; }
+	public partial class Dog : Obstacle
+	{
+		public Vector2I Direction { get; private set; }
 
-    
-    public override void _Ready()
-    {
-        base._Ready();
-        
-        Tile lTargetTile = Grid.IndexDict[Grid.IndexDict[CurrentTile] + Direction];
 
-        lTargetTile?.SetDog(true);
-    }
+		public override void _Ready()
+		{
+			base._Ready();
+	        
+			Tile lTargetTile = Grid.IndexDict[Grid.IndexDict[CurrentTile] + Direction];
+
+			lTargetTile?.SetDog(true);
+		}
+
+		
     
-    public static Dog Create(PackedScene pScene, Tile pTile, Vector2I pDirection)
-    {
-        Dog lDog = Create<Dog>(pScene, pTile);
-        lDog.Direction = pDirection.Sign();
-        return lDog;
-    }
+		public static Dog Create(PackedScene pScene, Tile pTile, Vector2I pDirection)
+		{
+			Dog lDog = Create<Dog>(pScene, pTile);
+			lDog.Direction = pDirection.Sign();
+			return lDog;
+		}
+	}
 }
