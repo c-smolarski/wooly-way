bb0e8a8a1a2c00d6e3d383c0577c39dd413dd68c
alissa110088 <delattre.alissa@gmail.com>
2025-03-15 12:03:00 +0100

    HighScore Scene creation

---
+++ PROJECT/Scenes/Highscores.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+// Author : Alissa Delattre
+
+namespace Com.IsartDigital.ProjectName {
+	
+	public partial class Highscores : Label
+	{
+		public override void _Ready()
+		{
+
+		}
+
+		public override void _Process(double pDelta)
+		{
+			float lDelta = (float)pDelta;
+
+		}
+
+		protected override void Dispose(bool pDisposing)
+		{
+
+		}
+	}
+}
